79131381b4e6fda4eca94a4a378846736b7d024b
lukaskazmierczak <lukas.kazman@gmail.com>
2026-07-24 14:41:28 -0400

    Update Hybrid_QKD_Model.py
    Corrected file imports

--- Hybrid_QKD_Model.py
+++ Hybrid_QKD_Model.py
@@ -23,6 +23,6 @@ from cirq import Z, PauliSum
 
 import sys
 sys.meta_path[:] = [f for f in sys.meta_path if "DaskFinder" not in str(f)]
-import ghz_group_b92_qkd.py
-import qkd_qith_ghz.py
-import quantum_coinflip.py
+import ghz_group_b92_qkd
+import qkd_qith_ghz
+import quantum_coinflip
