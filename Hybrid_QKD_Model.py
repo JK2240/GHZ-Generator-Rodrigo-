a8c07f533334767c35e9fbceda82fd80202931ac
lukaskazmierczak <lukas.kazman@gmail.com>
2026-07-27 15:25:53 -0400

    Modified code for VSCode use

--- Hybrid_QKD_Model.py
+++ Hybrid_QKD_Model.py
@@ -17,17 +17,17 @@ from cirq import Z, PauliSum
 import sys
 sys.meta_path[:] = [f for f in sys.meta_path if "DaskFinder" not in str(f)]
 import ghz_group_b92_qkd
-import qkd_qith_ghz
+import QKD_with_GHZ
 import quantum_coinflip
 
 count = 0
-Final = {}
+final = {}
 B92 = {run_B92(30)}
-QKD = {run_simulation(num_bits = 30, eavesdropped = False)
+QKD = {run_simulation(num_bits = 30, eavesdropped = False)}
 for i in range(30):
   if B92(i) == 0 and QKD(i) == 0:
-    final(count) = 0
+    final[count] = 0
     count = count + 1
   elif B92(i) == 1 and QKD(i) == 1:
-    final(count) = 1
+    final[count] = 1
     count = count + 1
