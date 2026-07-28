335b82b3fcc55e47119c06bd93a0bd0392bbe386
lukaskazmierczak <lukas.kazman@gmail.com>
2026-07-28 14:44:19 -0400

    Fixed old bad code

--- Hybrid_QKD_Model.py
+++ Hybrid_QKD_Model.py
@@ -22,11 +22,7 @@ from quantum_coinflip import *
 
 count = 0
 final = []
-B92 = []
-B92_add = run_B92(30)
-for j in range(len(B92_add)):
-  B92[j] = B92_add[j]
-
+B92 = [run_B92(30)]
 QKD = [run_simulation(num_bits = len(B92), eavesdropped = False)]
 for i in range(30):
   if B92[i] == 0 and QKD[i] == 0:
