850ae4edf3a6da838018b5fbfa28c453b1f213ed
lukaskazmierczak <lukas.kazman@gmail.com>
2026-07-28 14:41:24 -0400

    Fixed list conflict in main

--- Hybrid_QKD_Model.py
+++ Hybrid_QKD_Model.py
@@ -22,7 +22,11 @@ from quantum_coinflip import *
 
 count = 0
 final = {}
-B92 = {run_B92(30)}
+B92 = {}
+B92_add = run_B92(30)
+for j in range(len(B92_add)){
+  B92[j] = B92_add[j]
+}
 QKD = {run_simulation(num_bits = len(B92), eavesdropped = False)}
 for i in range(30):
   if B92[i] == 0 and QKD[i] == 0:
